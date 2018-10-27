b87bb6d897a3b5b6d72dc88c4e7b0c7a779af176
MathPe <pellegrin.mathieu@gmail.com>
2018-10-27 20:18:15 +0200

    First commit

---
+++ hello/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from hello.models import Post
+
+admin.site.register(Post)
---
+++ hello/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from hello.models import Post
+
+def myView(request):
+	context = {
+        'posts': Post.objects.all() # Query datas from DB
+	}
+	return render(request, 'hello/base.html', context)
